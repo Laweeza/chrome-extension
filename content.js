15385fd05a2ebde3a4c49efacd2db9e3db3bf24f
Laweeza <louieshing@gmail.com>
2022-07-18 17:41:44 -0700

    Added randomization for cursors

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
-let imagePath = chrome.runtime.getURL('assets/pusheen.png');
+window.onload = () => {
+  let imagePath = chrome.runtime.getURL('assets/pusheen.png');
+  let imagePath2 = chrome.runtime.getURL('assets/fin.png');
+  let array = [imagePath, imagePath2];
 
-window.onload = function () {
-  document.body.style.cursor = `url(${imagePath}), pointer`;
+  function getRandom() {
+    return Math.floor(Math.random() * (2 - 0) + 0);
+  }
+
+  document.body.style.cursor = `url(${array[getRandom()]}), auto`;
 };
