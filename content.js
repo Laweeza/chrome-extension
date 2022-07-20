d94d55e3f92f4ad0272f0ee9523df89bb40f3c86
Laweeza <louieshing@gmail.com>
2022-07-20 14:17:20 -0700

    Added typescript

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 window.onload = () => {
   let imagePath = chrome.runtime.getURL('assets/pusheen.png');
   let imagePath2 = chrome.runtime.getURL('assets/fin.png');
-  let array = [imagePath, imagePath2];
+  let imagePath3 = chrome.runtime.getURL('assets/pikachu.png');
+  let array = [imagePath, imagePath2, imagePath3];
 
   function getRandom() {
-    return Math.floor(Math.random() * (2 - 0) + 0);
+    return Math.floor(Math.random() * (3 - 0) + 0);
   }
 
   document.body.style.cursor = `url(${array[getRandom()]}), auto`;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.tsx',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'popup.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
