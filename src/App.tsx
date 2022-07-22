2529a30afb8a08fe6aceb280df0104a9e071980e
Laweeza <louieshing@gmail.com>
2022-07-21 17:12:50 -0700

    Added container

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
 import * as React from 'react';
+import styled from 'styled-components';
 import Sad from '../assets/sad.svg';
 
 export default function App() {
   return (
-    <h1>
-      <Sad />
-    </h1>
+    <Container>
+      <Screen />
+      {/* <Sad /> */}
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+  background-color: #8ae9c1;
+  box-sizing: border-box;
+  border: solid;
+  border-radius: 8px;
+  max-width: 300px;
+  min-height: 580px;
+  margin-right: auto;
+`;
+
+const Screen = styled.div`
+  border: inherit;
+  background-color: #a0eec0;
+  border-radius: inherit;
+  min-width: calc(100% - 100px);
+  max-height: 100px;
+  margin-top: 20px;
+`;
